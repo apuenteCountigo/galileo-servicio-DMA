75e07e10c624088f045e4e9c1546aacdfa4bea6b
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-02 05:36:11 -0400

    INIT

---
+++ servicio-dma/src/test/java/com/galileo/cu/servicio_dma/ServicioDmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galileo.cu.servicio_dma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioDmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
