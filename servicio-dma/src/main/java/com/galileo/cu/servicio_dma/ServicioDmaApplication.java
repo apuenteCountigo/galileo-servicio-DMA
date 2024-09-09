12706e1798a285ebfca36981bd49d95a42b695bf
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-09 15:55:02 -0400

    :::

--- servicio-dma/src/main/java/com/galileo/cu/servicio_dma/ServicioDmaApplication.java
+++ servicio-dma/src/main/java/com/galileo/cu/servicio_dma/ServicioDmaApplication.java
@@ -20,6 +20,6 @@ public class ServicioDmaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("DMA V-240901544");
+		System.out.println("DMA V-240901554");
 	}
 }
