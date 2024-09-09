dedbab1b89dcb325e46805ed3e095f310ea27534
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-09 12:43:31 -0400

    :

--- servicio-dma/src/main/java/com/galileo/cu/servicio_dma/ServicioDmaApplication.java
+++ servicio-dma/src/main/java/com/galileo/cu/servicio_dma/ServicioDmaApplication.java
@@ -1,29 +1,25 @@
 package com.galileo.cu.servicio_dma;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import com.galileo.cu.servicio_dma.clients.*;
-import com.galileo.cu.servicio_dma.dto.*;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @EnableDiscoveryClient
 @EnableFeignClients
 @EntityScan({ "com.galileo.cu.commons.models" })
 @SpringBootApplication
 public class ServicioDmaApplication implements CommandLineRunner {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ServicioDmaApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		log.info("**************************************");
-		log.info("DMA V-2409091111");
+		System.out.println("**************************************");
+		System.out.println("DMA V-2409030608");
 	}
 }
