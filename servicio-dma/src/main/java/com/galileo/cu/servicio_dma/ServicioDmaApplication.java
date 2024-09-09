d20b8fea8cecca0fc0597e3fdb4643b074a7f558
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-09 18:42:08 -0400

    INIT

---
+++ servicio-dma/src/main/java/com/galileo/cu/servicio_dma/ServicioDmaApplication.java
@@ -0,0 +1,26 @@
+package com.galileo.cu.servicio_dma;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EntityScan({ "com.galileo.cu.commons.models" })
+public class ServicioDmaApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioDmaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("**************************************");
+		System.out.println("DMA V-2409091840");
+	}
+
+}
