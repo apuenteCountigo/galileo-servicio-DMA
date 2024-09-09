57066f690e8da62da0e3b4d5389f8cea5ba3bd0a
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-09 16:21:16 -0400

    escaneo de paquetes

--- servicio-dma/src/main/java/com/galileo/cu/servicio_dma/ServicioDmaApplication.java
+++ servicio-dma/src/main/java/com/galileo/cu/servicio_dma/ServicioDmaApplication.java
@@ -6,11 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+// @EntityScan({ "com.galileo.cu.commons.models" })
+// @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EntityScan({ "com.galileo.cu.commons.models" })
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.galileo.cu")
+@EntityScan(basePackages = "com.galileo.cu.commons.models")
+@EnableJpaRepositories(basePackages = "com.galileo.cu.servicio_dma.repositories")
 public class ServicioDmaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
